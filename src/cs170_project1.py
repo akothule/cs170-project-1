cae35df29a881637dc58d768765e1007f762c556
Ayush <68819610+akothule@users.noreply.github.com>
2025-01-28 10:58:38 -0800

    Added input retrieval and basic validation

--- src/cs170_project1.py
+++ src/cs170_project1.py
@@ -2,15 +2,44 @@ from heuristics import Heuristics
 
 
 def main():
-    # take user input for initial state
-    initial_state = input("Enter an initial state: ")
+    while (True):
+        # take user input for initial state
+        print("Enter a valid initial state. Separate each number with a comma and a space. Use 0 to represent a blank space")
+        initial_state = []
+        initial_state.append(input("Enter the first row: "))
+        initial_state.append(input("Enter the second row: "))
+        initial_state.append(input("Enter the third row: "))
+
+        for i in range(0, 3):
+            # split each row into an array of 3 elements
+            initial_state[i] = initial_state[i].split(", ")
+            # convert each string array to int array
+            initial_state[i] = [int(x) for x in initial_state[i]]
+
+        checkInput = True
+        # check if input is valid
+        # doesn't check if it can be solved, only checks if there is 1 of each number from 0-8
+        for i in range(0, 9):
+            # check count of each number is 1
+            count = initial_state[0].count(i) + initial_state[1].count(i) + initial_state[2].count(i)
+            if count != 1:
+                checkInput = False
+
+        if checkInput:
+            break
+        else:
+            print("Invalid Input. Initial state must contain one of each number from 1-8 with 0 as a blank")
+
+
 
     heuristics = Heuristics(initial_state)
 
+    heuristics.print_state(initial_state)
+
     # call all 3 algorithms
     heuristics.uniform_cost_search()
-    heuristics.aStar_misplaced_tile()
-    heuristics.aStar_manhattan_distance()
+    heuristics.a_star_misplaced_tile()
+    heuristics.a_star_manhattan_distance()
 
     return 0
 
--- src/heuristics.py
+++ src/heuristics.py
@@ -5,9 +5,15 @@ class Heuristics:
     def __init__(self, initial_state: List[List[int]]):
         self.initial_state = initial_state
 
+        self.current_state = initial_state
+
         self.goal_state = [[1, 2, 3],
                            [4, 5, 6],
                            [7, 8, 0]]
+
+    def print_state(self, state: List[List[int]]):
+        for i in range(0, 3):
+            print(state[i])
 
     def uniform_cost_search(self):
         return 0
